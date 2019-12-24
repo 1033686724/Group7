fa09c5a6186cf66cab55539fbc08729263dcf81c
xwhswd <xwhswd@163.com>
2019-12-24 17:13:04 +0800

    xwh

---
+++ src/main/java/com/briup/group/web/controller/XwhController.java
@@ -0,0 +1,17 @@
+package com.briup.group.web.controller;
+
+import com.briup.group.util.Message;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class XwhController {
+    public Message play(){
+        Message message=new Message();
+        message.setTime(System.currentTimeMillis());
+        message.setStatus(0);
+        message.setMessage("xwh");
+        message.setData("有点意思");
+        return message;
+    }
+
+}
