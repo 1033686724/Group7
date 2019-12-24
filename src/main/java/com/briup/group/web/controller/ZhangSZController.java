db53f3eaf082740baf9f9f3964639d11bf5816f6
zhangsz <932087228@qq.com>
2019-12-24 17:08:11 +0800

    小章的控制类书写

--- src/main/java/com/briup/group/web/controller/ZhangSZController.java
+++ src/main/java/com/briup/group/web/controller/ZhangSZController.java
@@ -1,4 +1,11 @@
 package com.briup.group.web.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ZhangSZController {
+      public String zsz(){
+
+          return "添加信息成功";
+      }
 }
