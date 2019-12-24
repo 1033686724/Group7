8f105b09207980040271862c0517980f1094287e
huangyf <1033686724@qq.com>
2019-12-24 15:32:19 +0800

    初始化项目

---
+++ src/main/java/com/briup/group/Group7Application.java
@@ -0,0 +1,13 @@
+package com.briup.group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Group7Application.class, args);
+    }
+
+}
