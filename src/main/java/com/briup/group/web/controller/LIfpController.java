3aaeb4c8b9c3094d15081956f7be861a25e30b2e
lifp <1016785274@qq.com>
2019-12-24 18:29:52 +0800

    控制器

--- src/main/java/com/briup/group/web/controller/LIfpController.java
+++ src/main/java/com/briup/group/web/controller/LIfpController.java
@@ -1,4 +1,14 @@
 package com.briup.group.web.controller;
 
+import com.briup.group.util.Message;
+import com.briup.group.util.MessageUtil;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LIfpController {
+    @PostMapping
+    public Message sdd(int stuID){
+        MessageUtil.success();
+    }
 }
