689375c4a85f96316336924039918dff4cde05b5
zhangsz <932087228@qq.com>
2019-12-25 08:54:40 +0800

    小章的在在在次修改

--- src/main/java/com/briup/group/web/controller/ZhangSZController.java
+++ src/main/java/com/briup/group/web/controller/ZhangSZController.java
@@ -1,13 +1,19 @@
 package com.briup.group.web.controller;
 
+import com.briup.group.util.Message;
+import com.briup.group.util.MessageUtil;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ZhangSZController {
-    @GetMapping("/zsz")
-    public String zsz(){
+    @GetMapping("/add")
+    public Message add(){
 
-          return "添加信息成功";
+          return MessageUtil.success("添加信息成功");
       }
+    public Message delete(){
+
+        return MessageUtil.success("删除信息成功");
+    }
 }
