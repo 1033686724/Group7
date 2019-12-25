dd74c8c008753d7264cd629594a10615ba501784
xwhswd <xwhswd@163.com>
2019-12-25 08:56:08 +0800

    小改

--- src/main/java/com/briup/group/web/controller/XwhController.java
+++ src/main/java/com/briup/group/web/controller/XwhController.java
@@ -1,19 +1,31 @@
 package com.briup.group.web.controller;
 
 import com.briup.group.util.Message;
+import com.briup.group.util.MessageUtil;
+import io.swagger.annotations.ApiImplicitParam;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/Xwh")
 public class XwhController {
-    @PostMapping("/xwh")
-    public Message play(){
-        Message message=new Message();
-        message.setTime(System.currentTimeMillis());
-        message.setStatus(0);
-        message.setMessage("xwh");
-        message.setData("有点意思");
-        return message;
+    @PostMapping("/add")
+    @ApiImplicitParam(name = "message",dataType = "String",value = "xwh的",paramType ="Query",required = true)
+    public Message add(){
+        return MessageUtil.success("添加成功");
+    }
+    @PostMapping("/del")
+    public Message del(){
+        return MessageUtil.success("删除成功");
+    }
+    @PostMapping("/change")
+    public Message change(){
+        return MessageUtil.success("更改成功");
+    }
+    @PostMapping("/search")
+    public Message search(){
+        return MessageUtil.success("查询成功");
     }
 
 }
